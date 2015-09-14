26f75eac2d36f4b68f642cd31f72866d7b81fd26
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2015-09-14 13:50:30 -0500

    cleaning up search

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
+var IntlMixin     = ReactIntl.IntlMixin;
+var FormattedDate = ReactIntl.FormattedDate;
+var FormattedRelative = ReactIntl.FormattedRelative;
+
+// Just adding this rather then pulling in all of underscore
+// http://modernjavascript.blogspot.com/2013/08/building-better-debounce.html
+var debounce = function(func, wait) {
+  // we need to save these in the closure
+  var timeout, args, context, timestamp;
+
+  return function() {
+
+    // save details of latest call
+    context = this;
+    args = [].slice.call(arguments, 0);
+    timestamp = new Date();
+
+    // this is where the magic happens
+    var later = function() {
+
+      // how long ago was the last call
+      var last = (new Date()) - timestamp;
+
+      // if the latest call was less that the wait period ago
+      // then we reset the timeout to wait for the difference
+      if (last < wait) {
+        timeout = setTimeout(later, wait - last);
+
+        // or if not we can null out the timer and run the latest
+      } else {
+        timeout = null;
+        func.apply(context, args);
+      }
+    };
+
+    // we only need to set the timer now if one isn't already running
+    if (!timeout) {
+      timeout = setTimeout(later, wait);
+    }
+  }
+};
 
 var ApiKeyForm = React.createClass({
 
@@ -75,17 +116,16 @@ var AccessHolder = React.createClass({
   }
 });
 
-
 var Door = React.createClass({
 
   render: function() {
     var door = this.props.doorState;
-    var doorIcon = 'door-' + door.key;
+    var doorIcon = 'door-' + door.door;
 
     return (
       <div id={doorIcon} className="door">
         <h2 className={door.relayState}>{door.doorName}</h2>
-        <DoorForm doorKey={door.key} onDoorSubmit={this.props.onDoorSubmit} />
+        <DoorForm doorKey={door.door} onDoorSubmit={this.props.onDoorSubmit} />
       </div>
     );
   }
@@ -106,12 +146,41 @@ var LakituResult = React.createClass({
   }
 });
 
+var CredentialSearch = React.createClass({
+  handleChange: function(e) {
+    this.props.handleSearch(
+      this.refs.query.getDOMNode().value
+    );
+  },
+
+  componentWillMount: function() {
+    this.lazyChange = debounce(this.handleChange, 200, function(e) {
+      e.preventDefault();
+    });
+  },
+
+  render: function() {
+    return (
+      <form className="find-access">
+        <h2>Find Access Holders</h2>
+        <input type="text"
+               className="cred-search"
+               placeholder="Search..."
+               ref="query"
+               value={this.props.query}
+               onChange={this.lazyChange} />
+      </form>
+    )
+  }
+});
+
+
 var DoorList = React.createClass({
   render: function() {
     var onDoorSubmit = this.props.onDoorSubmit;
-    var doorNodes = this.props.data.map(function(door) {
+    var doorNodes = this.props.doors.map(function(door) {
       return (
-          <Door key={door.key} doorState={door} onDoorSubmit={onDoorSubmit} />
+          <Door key={door.door} doorState={door} onDoorSubmit={onDoorSubmit} />
       );
     });
     return (
@@ -145,6 +214,157 @@ var DoorForm = React.createClass({
 
 });
 
+var EventList = React.createClass({
+  render: function() {
+    var eventNodes = this.props.events.map(function(event) {
+      return (
+          <Event key={event.key}
+                 door={event.door}
+                 eventType={event.eventType}
+                 firstName={event.forename}
+                 lastName={event.surname}
+                 timestamp={event.timestamp} />
+      );
+    });
+    return (
+      <div className="eventList">
+        {eventNodes}
+      </div>
+    );
+  }
+});
+
+var Cardholders = React.createClass({
+  render: function() {
+    var eventNodes = this.props.cardholders.map(function(cardholder) {
+      return (
+        <Cardholder key={cardholder.key}
+                    door={cardholder.door}
+                    cardholderID={cardholder.cardholderID}
+                    firstName={cardholder.forename}
+                    middleName={cardholder.middleName}
+                    lastName={cardholder.surname}
+                    email={cardholder.email}
+                    phone={cardholder.phone}
+                    credentials={cardholder.credentials}
+                    roles={cardholder.roles} />
+      );
+    });
+
+    if (this.props.cardholders.length > 0) {
+      return (
+        <div className="cardholders">{eventNodes}</div>
+      );
+    } else {
+      return (<span/>)
+    }
+
+  }
+});
+
+var Cardholder = React.createClass({
+  render: function() {
+    var name = this.props.firstName;
+    if (this.props.middleName) {
+      name = name + ' ' + this.props.middleName;
+    }
+    if (this.props.lastName) {
+      name = name + ' ' + this.props.lastName;
+    }
+
+    var cardholder = [];
+    cardholder.push( <span key="key" className="key">{this.props.key}</span> );
+    if (name) { cardholder.push( <span key="name" className="name">{name}</span> ); }
+    if (this.props.email) { cardholder.push( <span key="email" className="email">{this.props.email}</span> ); }
+    if (this.props.phone) { cardholder.push( <span key="phone" className="phone">{this.props.phone}</span> ); }
+
+    return (
+      <div className="cardholder">
+        {cardholder}
+         <Roles roles={this.props.roles}/>
+         <Credentials credentials={this.props.credentials} />
+      </div>
+    )
+  }
+});
+
+var Credentials = React.createClass({
+  render: function() {
+    if (!this.props.credentials) { return(<span/>); };
+    var credentialNodes = this.props.credentials.map(function(credential) {
+      return (
+        <Credential key={credential.credentialID}
+                    cardNumber={credential.cardNumber}
+                    endTime={credential.endTime} />
+      );
+    });
+    return (
+      <ul className="credentials">
+        {credentialNodes}
+      </ul>
+    );
+  }
+});
+
+var Credential = React.createClass({
+  mixins: [IntlMixin],
+  render: function() {
+    var credential = [];
+    credential.push( <span key="cardNumber" className="cardNumber">{this.props.cardNumber} </span> );
+
+    if (this.props.endTime) {
+      var expires = new Date(this.props.endTime);
+      credential.push( <span key="expires" className="expires"><FormattedDate value={expires} /></span> );
+    }
+    return (
+      <li className="credential">{credential}</li>
+    );
+  }
+});
+
+
+var Roles = React.createClass({
+  render: function() {
+    if (!this.props.roles) { return(<span/>); };
+    var schedules = this.props.roles.map(function(role) {
+      return (
+        <Schedule key={role.roleID} scheduleName={role.scheduleName} />
+      );
+    });
+    return (
+      <span className="roleList">
+        {schedules}
+      </span>
+    );
+  }
+});
+
+var Schedule = React.createClass({
+  render: function() {
+    return (
+        <span className="schedule">{this.props.scheduleName}</span>
+    );
+  }
+});
+
+
+var Event = React.createClass({
+  render: function() {
+    var desc = this.props.eventType;
+    if (this.props.firstName) {
+      desc = this.props.firstName + ' ' + this.props.lastName;
+    }
+    return (
+      <div className="event">
+         <span className="door"> {this.props.door} </span> 
+         <span className="desc"> {desc} </span> 
+         <span className="time">
+           <FormattedRelative value={this.props.timestamp} units="hour" />
+         </span>
+      </div>
+    );
+  }
+});
 
 var DoorBox = React.createClass({
 
@@ -152,23 +372,14 @@ var DoorBox = React.createClass({
     var url = this.props.lakituUrl + 'doors';
     var apiToken = this.state.access.token;
 
+    if (!apiToken) { return; }
+
     $.getJSON(url, {access_token: apiToken})
     .done(function(json) {
-      var doors = [];
-      for (var key in json) {
-        if (json.hasOwnProperty(key)) {
-          var door = json[key];
-          door.key = key;
-          doors.push(door);
-        }
-      }
-      this.apiTokenValid("valid");
-      this.setState({
-        commandResult: this.state.commandResult,
-        access: this.state.access,
-        data: doors
-      });
 
+      var doors = this.mapToList(json, function(door) { return door.door; });
+      this.apiTokenValid("valid");
+      this.setState({ doors: doors });
     }.bind(this))
     .fail(function(xhr, status, err) {
       this.apiTokenValid("invalid");
@@ -176,21 +387,75 @@ var DoorBox = React.createClass({
     }.bind(this))
   },
 
-  handleApiKeySubmit: function(apiToken) {
-    this.setState({
-      commandResult: this.state.commandResult,
-      access: { token: apiToken, success: "unknown" },
-      data: this.state.data
-    });
+  loadEventsFromServer: function() {
+    var apiToken = this.state.access.token;
+    var url = this.props.lakituUrl + 'events';
+    if (!apiToken) { return; }
+
+    $.getJSON(url, {access_token: apiToken})
+    .done(function(json) {
+      var events = this.mapToList(json, function(event) {
+        return event.timestamp + '@' + event.door;
+      });
+      events.sort(function(a, b) {
+        if ( a.timestamp < b.timestamp ) return 1;
+        if ( a.timestamp > b.timestamp ) return -1;
+        return 0;
+      });
+      this.setState({ events: events });
+    }.bind(this))
+    .fail(function(xhr, status, err) {
+      console.error(url, status, err.toString());
+    }.bind(this))
   },
 
-  apiTokenValid: function(success) {
-    var token = this.state.access.token;
-      this.setState({
-        commandResult: this.state.commandResult,
-        access: { token: token, success: success },
-        data: this.state.data
+  findCardholders: function(query) {
+    if (!query) { return; }
+
+    var apiToken = this.state.access.token;
+    var url = this.props.lakituUrl + 'cardholders/find/' + query;
+    if (!apiToken) { return; }
+
+    $.getJSON(url, {access_token: apiToken})
+    .done(function(json) {
+
+      var cardholders = this.mapToList(json, function(cardholder) {
+        return cardholder.cardholderID + ':' + cardholder.door;
       });
+      cardholders.sort(function(a, b) {
+        if ( (a.forename + a.surname) < (b.forename + b.surname) ) return -1;
+        if ( (a.forename + a.surname) > (b.forename + b.surname) ) return 1;
+        return 0;
+      });
+
+      this.setState({ cardholders: cardholders });
+    }.bind(this))
+    .fail(function(xhr, status, err) {
+      console.error(url, status, err.toString());
+    }.bind(this))
+  },
+
+  mapToList: function(json, getKey) {
+      var things = [];
+      for (var door in json) {
+        if (json.hasOwnProperty(door)) {
+          var stuff = json[door];
+          if( Object.prototype.toString.call( stuff ) === '[object Array]' ) {
+            for (var i in stuff) {
+              var thing = stuff[i];
+              thing.door = door;
+              thing.key = getKey(thing);
+              things.push(thing);
+            }
+          } else {
+            stuff.door = door;
+            stuff.key = getKey(stuff);
+            things.push(stuff);
+          }
+        }
+      }
+
+      return things;
   },
 
   handleDoorSubmit: function(doorCommand) {
@@ -198,19 +463,11 @@ var DoorBox = React.createClass({
     var url = this.props.lakituUrl +
               'doors/' + doorCommand.action + '/' + doorCommand.door;
 
-    this.setState({
-      commandResult: { messageId: "", md5OfMessageBody: this.state.commandResult.md5OfMessageBody },
-      access: this.state.access,
-      data: this.state.data
-    });
+    this.clearMessageId();
 
     $.post(url, {access_token: apiToken})
       .done(function(json) {
-        this.setState({
-          commandResult: json,
-          access: this.state.access,
-          data: this.state.data
-        });
+        this.setState({ commandResult: json });
         this.apiTokenValid("valid");
       }.bind(this))
       .fail(function(xhr, status, err) {
@@ -218,17 +475,11 @@ var DoorBox = React.createClass({
       }.bind(this));
   },
 
-  getInitialState: function() {
-    return {
-      data: [],
-      access: { token: "", success: "unknown" },
-      commandResult: { md5OfMessageBody: "", messageId:  "" }
-    };
-  },
-
   componentDidMount: function() {
     this.loadDoorsFromServer();
+    this.loadEventsFromServer();
     setInterval(this.loadDoorsFromServer, this.props.pollInterval);
+    setInterval(this.loadEventsFromServer, this.props.pollInterval);
   },
 
   handleAccessHolder: function(accessHolder) {
@@ -241,10 +492,42 @@ var DoorBox = React.createClass({
       <div className="doorBox">
         <LakituResult commandResult={this.state.commandResult} />
         <ApiKeyForm access={this.state.access} onApiKeySubmit={this.handleApiKeySubmit} />
-        <DoorList data={this.state.data} onDoorSubmit={this.handleDoorSubmit} />
+        <DoorList doors={this.state.doors} onDoorSubmit={this.handleDoorSubmit} />
+        <EventList events={this.state.events} />
         <AccessHolder onAccessHolderSubmit={this.handleAccessHolder} />
+        <CredentialSearch handleSearch={this.findCardholders} />
+        <Cardholders cardholders={this.state.cardholders}/>
       </div>
     );
+  },
+
+  handleApiKeySubmit: function(apiToken) {
+    this.setState(
+      { access: { token: apiToken, success: "unknown" } },
+      function() {
+        this.loadDoorsFromServer();
+        this.loadEventsFromServer();
+      });
+  },
+
+  apiTokenValid: function(success) {
+    this.setState({ access: { token: this.state.access.token, success: success } });
+  },
+
+  clearMessageId: function() {
+    this.setState({
+      commandResult: { messageId: "", md5OfMessageBody: this.state.commandResult.md5OfMessageBody },
+    });
+  },
+
+  getInitialState: function() {
+    return {
+      access: { token: "", success: "unknown" },
+      commandResult: { md5OfMessageBody: "", messageId:  "" },
+      cardholders: [],
+      doors: [],
+      events: []
+    };
   }
 
 });
