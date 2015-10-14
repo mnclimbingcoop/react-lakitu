f6e7b068f944e8ce3a3d1109e66a14afcbdecab2
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2015-10-14 14:20:56 -0500

    fixing publish task

--- src/gulp/tasks/publish.js
+++ src/gulp/tasks/publish.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var s3 = require('gulp-s3');
+var fs = require('fs');
+
+aws = JSON.parse(fs.readFileSync('./aws.json'));
 
 gulp.task('publish', function(){
   gulp.src('./dist/**')
