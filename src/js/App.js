55bb87d6e736cea6155768bbb408472104ff05a6
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2015-10-14 14:09:51 -0500

    using internal IP for doors locally

--- src/js/App.js
+++ src/js/App.js
@@ -5,6 +5,6 @@ var Lakitu = require('./Lakitu');
 
 React.render(
   <Lakitu pollInterval={3000}
-          lakituUrl="http://mncc.aaronzirbes.com:6590/" />,
+          lakituUrl="http://night-watchman.aaronzirbes.com:6590/" />,
   document.getElementById('app')
 );
